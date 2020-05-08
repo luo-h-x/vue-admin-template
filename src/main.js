95c78c3ec984888f27fb7c2d4bed148a40f8d734
luo-h-x <1877430627@qq.com>
2020-05-08 14:00:42 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import './mock'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   router,
