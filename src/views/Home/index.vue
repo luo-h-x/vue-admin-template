e41c4a5cb76485680f0ec36b900fdfc57aed4df1
luo-h-x <1877430627@qq.com>
2020-05-08 14:06:32 +0800

    index.vue

---
+++ src/views/Home/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="height: 100%">
+    <el-container style="height: 100%">
+      <el-aside width="200px">
+        <Sidebar/>
+      </el-aside>
+      <el-container>
+        <el-header style="padding: 0">
+          <Navbar/>
+        </el-header>
+        <el-main style=" padding: 0">
+          <app-main/>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
+import AppMain from './components/AppMain'
+export default {
+  components: { Sidebar, Navbar, AppMain }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
