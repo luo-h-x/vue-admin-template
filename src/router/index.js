594358691628e8cd11da9f2e92284812b29b3439
luo-h-x <1877430627@qq.com>
2020-05-08 14:01:28 +0800

    routerjs

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,81 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/Login',
+    name: 'Login',
+    component: () => import('../views/Login')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    name: 'Home',
+    component: Home,
+    redirect: '/Admin',
+    children: [
+      {
+        path: '/Admin',
+        name: 'Admin',
+        component: () => import('../views/Admin')
+      }
+    ]
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('../views/404')
+  },
+  {
+    path: '/from',
+    component: Home,
+    children: [
+      {
+        path: '/From',
+        component: () => import('../views/From')
+      }
+    ]
+  },
+  {
+    path: '/Data',
+    name: 'Data',
+    component: Home,
+    redirect: '/Data/Tree',
+    children: [
+      {
+        path: 'Table',
+        name: 'Table',
+        component: () => import('../views/Table')
+      },
+      {
+        path: 'Tree',
+        name: 'Tree',
+        component: () => import('../views/Tree')
+      }
+    ]
+  },
+  {
+    path: '/error',
+    component: Home,
+    children: [
+      {
+        path: '404',
+        name: '404',
+        component: () => import('../views/404')
+      }
+    ]
+  },
+  {
+    path: '/Icon',
+    component: Home,
+    children: [
+      {
+        path: 'index',
+        component: () => import('../views/Icon')
+      }
+    ]
   }
 ]
 
