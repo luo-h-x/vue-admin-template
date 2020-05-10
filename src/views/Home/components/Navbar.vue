c31dbffeef687d389d84353676342007573abb23
luo-h-x <1877430627@qq.com>
2020-05-10 15:00:18 +0800

    navbar.vue

--- src/views/Home/components/Navbar.vue
+++ src/views/Home/components/Navbar.vue
@@ -14,9 +14,9 @@
     </div>
     <el-breadcrumb separator="/">
     <transition-group name="breadcrumb">
-      <el-breadcrumb-item v-for="(item,index) in levelList" :key="item.path">
-        <span v-if="index==levelList.length-1" class="no-redirect">{{ item.meta.title }}</span>
-        <a v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
+      <el-breadcrumb-item v-for="(item) in levelList" :key="item.path">
+        <span  class="no-redirect">{{ item.meta.title }}</span>
+        <!-- <a v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a> -->
       </el-breadcrumb-item>
     </transition-group>
     </el-breadcrumb>
